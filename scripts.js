1653389e1c515da739788c61fcc8535aab5826dd
Rob W <robertzwinchell@gmail.com>
2018-01-31 08:18:12 -0700

    Focus responsive

--- scripts.js
+++ scripts.js
@@ -1,6 +1,19 @@
 (function() {
 "use strict";
 
+
+var mobileDetect = function() {
+
+    var check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+
+    return check;
+
+};
+
+var isMobile = false;
+
+
 function renderMap(display) {
 	var mapArr = display.map;
 
@@ -229,6 +242,8 @@ var CanvasDisplay = function (parent, level, elementClass, listener) {
 		listener = this.cx;
 	}
 
+	isMobile = mobileDetect();
+
 	//add listeners
 	// listener.canvas.addEventListener("mousedown", this.selectMouseDown.bind(this));
 	listener.canvas.addEventListener("click", this.mapSelect.bind(this));
@@ -238,8 +253,11 @@ var CanvasDisplay = function (parent, level, elementClass, listener) {
 	//this.cx.canvas.addEventListener("DOMMouseScroll", this.mapZoom.bind(this));
 
 	//touch events
-	//this.cx.canvas.addEventListener("touchstart", this.mapTouchStart.bind(this));
-	//this.cx.canvas.addEventListener("touchmove", this.mapMoveTouch.bind(this));
+	if(isMobile){
+        this.cx.canvas.addEventListener("touchstart", this.mapTouchStart.bind(this));
+	// 	this.cx.canvas.addEventListener("touchmove", this.mapMoveTouch.bind(this));
+	}
+
 
 	this.touch = {
 		x: null,
@@ -476,6 +494,62 @@ CanvasDisplay.prototype.mapSelect = function (event) {
 	event.preventDefault();
 };
 
+
+CanvasDisplay.prototype.mapTouchStart = function (touchEvent) {
+	var canvas,
+		clickLoc,
+		pos,
+		tileLoc;
+
+	if (this.view.move === true) {
+		this.view.move = false;
+		return null;
+	}
+
+
+    var touches = touchEvent.changedTouches;
+
+    var event = touches[0];
+
+    console.log('touch event');
+
+
+	canvas = this.cx.canvas;
+
+	pos = this.relativePos(event, canvas);
+    
+	clickLoc = findTile(pos, this.view, this.level.scale / 2);
+
+	tileLoc = {
+		x: clickLoc.x / 2,
+		y: clickLoc.y / 2
+	};
+
+	// if (validTile(this.map, tileLoc)) {//this is suppose to select dots
+	// 	//this.view.select = tileLoc;
+	// } else
+	if (
+		tileLoc.x <= this.level.width - 1 &&
+		tileLoc.y <= this.level.height - 1 &&
+		clickLoc.x % 2 + clickLoc.y % 2 < 2
+		) {
+		if (!players.computerPlayerOn || players.computerPlayerOn && this.activePlayer === "player1"){
+			this.connectionToggle(clickLoc);
+		}
+	} else {
+		this.view.select = {};
+		return null;
+	}
+
+	renderMap(this);
+
+	if(players.computerPlayerOn && this.activePlayer !== "player1") {
+		this.computerPlayer();
+	}
+
+	touchEvent.preventDefault();
+};
+
 CanvasDisplay.prototype.updateScore = function (player) {
 	document.getElementById(player + "score").innerHTML = ++this.score[player];
 	this.endGame();
@@ -630,23 +704,23 @@ function toggleModal(modalToClose) {
 }
 window.turnModalOff = function (modalToClose) {
 //	alert ("turnModalOff start, modalToClose = " + modalToClose);
-	if (!document.getElementById(modalToClose).classList.contains("hide")) 
+	if (!document.getElementById(modalToClose).classList.contains("hide"))
 	{
     	document.getElementById(modalToClose).classList.add("hide");
 //		alert ("turnModalOff turned off");
 	}
-//	alert ("turnModalOff end");	
+//	alert ("turnModalOff end");
 };
 
 window.toggleModal = toggleModal;
 window.resetSettings = function () {
 	resetMap('beginner', 4);
-	resetPlay('play-against');	
+	resetPlay('play-against');
 	resetPlayerNames();
 };
 window.setCharacter = function (characterId) {
 	var min=0;
-	var max=12;  
+	var max=12;
 	var imageNum = Math.floor(Math.random() * (max - min + 1)) + min;
 //	alert ("setCharacter = " + imageNum);
 
@@ -658,18 +732,18 @@ window.setCharacter = function (characterId) {
 			imageNum =  Math.floor(Math.random() * (max - min)) + min;
 		}
 	}
-	
+
 	document.getElementById(characterId).innerHTML = "<img src='characters/" + imageNum + ".jpg' />";
 };
 function getLastImageNum (characterId) {
-	var lastImage = document.getElementById(characterId).innerHTML;	
+	var lastImage = document.getElementById(characterId).innerHTML;
 	var lastImageNum = lastImage.substring(21,23);
 	if (isNaN(lastImageNum.charAt(1)))
 	{
 		lastImageNum = lastImageNum.charAt(0);
 	}
-	
-	return lastImageNum;	
+
+	return lastImageNum;
 }
 window.resetMap = function (level, xtiles, ytiles) {
 //	alert ("resetMap, level = " + level + ", xtiles = " + xtiles + ", ytiles = " + ytiles);
@@ -678,28 +752,28 @@ window.resetMap = function (level, xtiles, ytiles) {
 		xtiles = "same";
 	else
 		resetLevel(level);
-		
-	if (xtiles === "same") {  
+
+	if (xtiles === "same") {
 		xtiles = lastLevel;
 		if (level=="same")
- 		{   
+ 		{
 			toggleModal("winner");
 			toggleModal("modal");
 		}
 	}
 
 	document.getElementById("redPlayerBox").classList.remove("largeGame");
-	document.getElementById("bluePlayerBox").classList.remove("largeGame");					
+	document.getElementById("bluePlayerBox").classList.remove("largeGame");
 
 //    if (level=="pro") {
 //		document.getElementById("redPlayerBox").classList.add("largeGame");
-//		document.getElementById("bluePlayerBox").classList.add("largeGame");			
+//		document.getElementById("bluePlayerBox").classList.add("largeGame");
 //	}
 	if (xtiles === "random") {
 		xtiles = (Math.random() * 13) + 7;
 		ytiles = (Math.random() * 13) + 7;
 		document.getElementById("redPlayerBox").classList.add("largeGame");
-		document.getElementById("bluePlayerBox").classList.add("largeGame");		
+		document.getElementById("bluePlayerBox").classList.add("largeGame");
 	}
 
 	lastLevel = xtiles > 7 ? "random" : xtiles;
@@ -708,12 +782,12 @@ window.resetMap = function (level, xtiles, ytiles) {
 	dotCanvas = new CanvasDisplay(document.getElementById("game"), newMap, "dot-canvas play-canvas");
 	document.getElementById("player1score").innerHTML = 0;
 	document.getElementById("player2score").innerHTML = 0;
-	
+
 	// add characters
 	setCharacter("redCharacter");
 	setCharacter("blueCharacter");
-		
-	
+
+
 	document.getElementById("scoreboard").classList.remove("player2-active");
 	document.getElementById("scoreboard").classList.add("player1-active");
 	renderMap(dotCanvas);
@@ -723,30 +797,30 @@ window.resetLevel = function (whichLevel) {
 //	alert ("resetLevel");
 	document.getElementById('beginner').classList.remove('active');
 	document.getElementById('intermediate').classList.remove('active');
-	document.getElementById('advanced').classList.remove('active');		
+	document.getElementById('advanced').classList.remove('active');
 	document.getElementById('pro').classList.remove('active');
-	document.getElementById(whichLevel).classList.add('active');	
+	document.getElementById(whichLevel).classList.add('active');
 }
 // window.resetPlay is new may 16
 window.resetPlay = function (playAgainst) {
 //	alert("resetPlay");
 	document.getElementById('play-against').classList.remove('active');
 	document.getElementById('play-against-no').classList.remove('active');
-	document.getElementById(playAgainst).classList.add('active');	
-		
+	document.getElementById(playAgainst).classList.add('active');
+
 	players.computerPlayerOn = document.getElementById('play-against').classList.contains('active');
 //	alert ("computerPlayerOn = " + players.computerPlayerOn);
 };
 window.resetPlayerNames = function () {
 //	document.getElementById('player1-name-input').value="";
 //	alert("resetPlayerNames 1");
-//	document.getElementById('player2-name-input').value="";	
+//	document.getElementById('player2-name-input').value="";
 
 //	p1Name="";
-//	p2Name="";	
+//	p2Name="";
 //	alert("reset p1Name and 2 = " + p1Name + ", " + p2Name);
 //	updateName.call(p1Name);
-//	alert("after updateName.call 1");	
+//	alert("after updateName.call 1");
 //	updateName(p2Name);
 //	alert("end of resetPlayerNames - player 1 = " + p1Name);
 };
@@ -827,7 +901,3 @@ window.onresize = function () {
 
 renderMap(dotCanvas);
 })();
-
-
-
-
